85739283cee9338628c070854222105130911b51
PetraNemcekova <101061242+PetraNemcekova@users.noreply.github.com>
2025-02-13 09:01:36 +0100

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,6 +1,16 @@
+var_type = type(variable)
+print(f'String to be shown {variable_value_to_be_shown}')
+
+# %% IMAGEIO
+import imageio.v3 as iio
+
+image = iio.imread('path_to_image')
+
 # %% MATPLOTLIB
 import matplotlib.pyplot as plt
-   
+
+image = plt.imread('path_to_image')
+
 plt.figure()
 plt.subplot(1,2,1)
 plt.imshow(image1)
@@ -21,6 +31,7 @@ plt.close('all')
 
 import numpy as np
 
+var_d_type = variable.dtype
 image = np.zeros_like(image)
 image = np.ones(x_size, y_size)
 img_shape = np.shape(image)
@@ -29,6 +40,12 @@ vector = np.linspace(start = start, stop = stop, num = number_of_elements)
 # %% SCIPY
 import scipy
 mat = scipy.io.loadmat('path_to_mat_file').get('label')
+
+# %% SIMPLE ITK
+import SimpleITK as sitk
+
+image = sitk.ReadImage('path_to_image')
+img_as_array = sitk.GetArrayFromImage(image)
 
 # %% SKIMAGE
 import skimage
